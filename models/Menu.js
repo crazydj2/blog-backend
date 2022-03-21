3ffb74a6d86205f31a6b5dab215f6060e618b055
crazydj2 <crazydj2@naver.com>
2022-03-21 16:43:53 +0900

    add models/Menu.js

---
+++ models/Menu.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+ 
+// Menu Schema
+const menuSchema = new Schema({
+    name: { type: String, required: true },
+    parent: { type: Schema.Types.ObjectId },
+    children: { type: [Schema.Types.ObjectId] },
+    articles: { type: [Schema.Types.ObjectId] }
+});
+
+// Menu Model
+// - 'menu' -> collection 명
+export default mongoose.model('menu', menuSchema);
