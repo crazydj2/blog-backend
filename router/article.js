71e5e059096b323749a0b3949576f5c5057357fd
crazydj2 <crazydj2@naver.com>
2022-07-05 14:12:18 +0900

    add test console

--- router/article.js
+++ router/article.js
@@ -8,6 +8,10 @@ const router = Router();
 // 모든 메뉴 데이터를 리스트로 리턴 (본디 트리 구조이긴 하나 이건 클라이언트에게 맡기자)
 router.get("/", async (req, res) => {
     console.log('get article......');
+
+    console.log(req.params);
+    console.log(req.query);
+    console.log(req.body);
     
     const data = await get(req.body);
 
@@ -18,6 +22,10 @@ router.get("/", async (req, res) => {
 // 하나의 메뉴 데이터 추가
 router.post("/", async (req, res) => {
     console.log('post article......');
+
+    console.log(req.params);
+    console.log(req.query);
+    console.log(req.body);
 
     const success = await create(req.body);
 
