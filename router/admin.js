ca1ed5cc9805046ec2e85420d1ee428424652f24
crazydj2 <crazydj2@naver.com>
2022-07-14 17:21:38 +0900

    add admin password

---
+++ router/admin.js
@@ -0,0 +1,31 @@
+
+import { Router } from 'express';
+import fs from "fs";
+
+const router = Router();
+
+// POST /admin
+// 하나의 메뉴 데이터 추가
+router.post("/", async (req, res) => {
+    console.log('post admin......');
+
+    let success = false;
+
+    try {
+        const password = fs.readFileSync("admin-password.dat", "utf8");
+
+        console.log(password);
+
+        if (req.body.password && req.body.password === password) {
+            success = true;
+        }
+    } catch (err) {
+        console.log(err);
+        success = false;
+    }
+
+    res.send({success});
+});
+
+
+export default router;
--- router/router.js
+++ router/router.js
@@ -1,8 +1,10 @@
 
 import menuRouter from './menu.js';
 import articleRouter from './article.js';
+import adminRouter from './admin.js';
 
 export const initRouter = app => {
     app.use('/menu', menuRouter);
     app.use('/article', articleRouter);
+    app.use('/admin', adminRouter);
 };
